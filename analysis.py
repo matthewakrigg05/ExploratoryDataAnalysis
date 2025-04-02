66e9387ed3a7bde1d7479f2c6f3c0d9db461a3d9
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-04-02 13:24:19 +0100

    docs: to analyse points

---
+++ analysis.py
@@ -0,0 +1,32 @@
+"""
+Points to analyse:
+    - Who are the most consistent high earners?
+    
+    - Which athlete has the highest cumulative earnings across all years?
+    
+    - How have the highest earnings changed over the decades?
+    
+    - What is the average earnings trend over time?
+    
+    - Is there an increase in earnings across all sports?
+    
+    - Which sport dominates the top earnings?
+    
+    - How do earnings vary between different sports?
+    
+    - Have new sports emerged in the rankings over time?
+    
+    - Nationalities of the Richest Athletes
+    
+    - Which countries produce the most top-earning athletes?
+    
+    - Is there a shift in dominance from certain countries over time?
+    
+    - Do top-ranked athletes stay at the top for multiple years?
+    
+    - What is the average rank change per athlete?
+    
+    - What is the gap between the top earner and the rest?
+    
+    - How evenly are earnings distributed among the top athletes?
+"""
