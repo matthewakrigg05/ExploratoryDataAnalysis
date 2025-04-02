b005d090177ed25d0629a319a79057f3f87aa7bb
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-04-02 13:31:54 +0100

    feat: preparing methods

--- analysis.py
+++ analysis.py
@@ -30,3 +30,53 @@ Points to analyse:
     
     - How evenly are earnings distributed among the top athletes?
 """
+
+class analysis:
+    
+    @staticmethod
+    def most_consistent_top_earners(df):
+        pass
+    
+    @staticmethod
+    def earnings_increase_over_decades(df):
+        pass
+    
+    @staticmethod
+    def average_highest_earnings(df):
+        pass
+    
+    @staticmethod
+    def increases_across_sports(df):
+        pass
+    
+    @staticmethod
+    def dominating_sport_in_earnings(df):
+        pass
+    
+    @staticmethod
+    def new_sports_in_rankings(df):
+        pass
+    
+    @staticmethod 
+    def top_earning_nationalities(df):
+        pass
+    
+    @staticmethod 
+    def changing_top_nationalities(df):
+        pass
+    
+    @staticmethod 
+    def consistency_of_highest_earners(df):
+        pass
+    
+    @staticmethod 
+    def average_change_in_ranking(df):
+        pass
+    
+    @staticmethod 
+    def gaps_in_earnings(df):
+        pass
+    
+    @staticmethod
+    def earning_distributions(df):
+        pass
